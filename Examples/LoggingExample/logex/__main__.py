f535debd2fa057a83f777fe6c8e3d449d144aed7
Matthias Hölzl <tc@xantira.com>
2022-07-13 15:10:18 +0200

    End of course

--- Examples/LoggingExample/logex/__main__.py
+++ Examples/LoggingExample/logex/__main__.py
@@ -3,13 +3,31 @@ import logging
 
 app = typer.Typer()
 
+
+def get_numeric_log_level(level: str):
+    numeric_level = getattr(logging, level.upper(), None)
+    if not isinstance(numeric_level, int):
+        raise ValueError("Invalid log level: {level!r}")
+    return numeric_level
+
+
 @app.command()
-def say_hi(name="world", num: int = 1):
-	logging.info(f"Said greeting to {name}")
-	for _ in range(num):
-		print(f"Hello, {name}!")
+def say_hi(name=typer.Argument("world"), log_level: str = "info"):
+
+    try:
+        logging.basicConfig(level=get_numeric_log_level(log_level))
+    except ValueError as err:
+        print(err)
+        logging.basicConfig(level=logging.INFO)
+
+    logging.debug(f"Called say_hi({name!r}, {log_level!r})")
+    logging.info(f"Said greeting to {name!r}.")
+    if name.strip() == "":
+        logging.warning("Name is empty!")
+        name = "world"
+
+    print(f"Hello, {name}!")
+
 
 if __name__ == "__main__":
-	logging.basicConfig(level=logging.INFO)
-	logging.info("Started logging example.")
-	app()
+    app()
